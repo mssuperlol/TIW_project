dc96315e2cc97e43462063ba766ea0c75c8689e8
mssuperlol <mssuperlol@gmail.com>
2025-05-06 17:58:55 +0200

    Added GenreDAO

---
+++ src/main/java/it/polimi/tiw_project/dao/GenreDAO.java
@@ -0,0 +1,35 @@
+package it.polimi.tiw_project.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenreDAO {
+    private final Connection connection;
+
+    public GenreDAO(Connection connection) {
+        this.connection = connection;
+    }
+
+    public List<String> getGenres() throws SQLException {
+        String query = "SELECT name FROM genres";
+
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (!resultSet.isBeforeFirst()) {
+                    return null;
+                }
+
+                List<String> genres = new ArrayList<>();
+                while (resultSet.next()) {
+                    genres.add(resultSet.getString("name"));
+                }
+
+                return genres;
+            }
+        }
+    }
+}
