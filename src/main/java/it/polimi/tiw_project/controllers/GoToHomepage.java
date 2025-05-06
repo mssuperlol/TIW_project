4b283df2563f35c09c1ce527c65b44c4d844ea8a
mssuperlol <mssuperlol@gmail.com>
2025-05-06 17:58:46 +0200

    Implemented initial homepage

--- src/main/java/it/polimi/tiw_project/controllers/GoToHomepage.java
+++ src/main/java/it/polimi/tiw_project/controllers/GoToHomepage.java
@@ -1,6 +1,11 @@
 package it.polimi.tiw_project.controllers;
 
+import it.polimi.tiw_project.beans.Playlist;
+import it.polimi.tiw_project.beans.User;
+import it.polimi.tiw_project.dao.GenreDAO;
+import it.polimi.tiw_project.dao.PlaylistDAO;
 import jakarta.servlet.ServletContext;
+import jakarta.servlet.UnavailableException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,25 +18,44 @@ import org.thymeleaf.templateresolver.WebApplicationTemplateResolver;
 import org.thymeleaf.web.servlet.JakartaServletWebApplication;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
 
 @WebServlet("/Homepage")
 public class GoToHomepage extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private TemplateEngine templateEngine;
+    private Connection connection = null;
 
     public GoToHomepage() {
         super();
     }
 
     @Override
-    public void init() {
-        ServletContext context = getServletContext();
-        JakartaServletWebApplication webApplication = JakartaServletWebApplication.buildApplication(context);
+    public void init() throws UnavailableException {
+        ServletContext servletContext = getServletContext();
+        JakartaServletWebApplication webApplication = JakartaServletWebApplication.buildApplication(servletContext);
         WebApplicationTemplateResolver templateResolver = new WebApplicationTemplateResolver(webApplication);
         templateResolver.setTemplateMode(TemplateMode.HTML);
         this.templateEngine = new TemplateEngine();
         this.templateEngine.setTemplateResolver(templateResolver);
         templateResolver.setSuffix(".html");
+
+        try {
+            ServletContext context = getServletContext();
+            String driver = context.getInitParameter("dbDriver");
+            String url = context.getInitParameter("dbUrl");
+            String user = context.getInitParameter("dbUser");
+            String password = context.getInitParameter("dbPassword");
+            Class.forName(driver);
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (ClassNotFoundException e) {
+            throw new UnavailableException("Can't load database driver");
+        } catch (SQLException e) {
+            throw new UnavailableException("Couldn't get db connection");
+        }
     }
 
     @Override
@@ -44,14 +68,39 @@ public class GoToHomepage extends HttpServlet {
             return;
         }
 
+        User user = (User) session.getAttribute("user");
+        PlaylistDAO playlistDAO = new PlaylistDAO(connection);
+        GenreDAO genreDAO = new GenreDAO(connection);
+        List<Playlist> playlists = null;
+        List<String> genres = null;
+
+        try {
+            playlists = playlistDAO.getPlaylists(user.getId());
+            genres = genreDAO.getGenres();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
         String path = "/WEB-INF/homepage.html";
         JakartaServletWebApplication webApplication = JakartaServletWebApplication.buildApplication(getServletContext());
         WebContext webC = new WebContext(webApplication.buildExchange(request, response), request.getLocale());
+        webC.setVariable("playlists", playlists);
+        webC.setVariable("genres", genres);
         templateEngine.process(path, webC, response.getWriter());
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         doGet(request, response);
+    }
+
+    @Override
+    public void destroy() {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException ignored) {
+        }
     }
 }
