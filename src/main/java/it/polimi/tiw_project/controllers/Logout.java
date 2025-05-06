4ce27ac826a7d19258afe42b87da405c2deed80f
mssuperlol <mssuperlol@gmail.com>
2025-05-06 09:26:28 +0200

    Implemented logout function

---
+++ src/main/java/it/polimi/tiw_project/controllers/Logout.java
@@ -0,0 +1,35 @@
+package it.polimi.tiw_project.controllers;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebServlet("/Logout")
+public class Logout extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public Logout() {
+        super();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        HttpSession session = request.getSession();
+
+        if(session != null) {
+            session.invalidate();
+        }
+
+        String path = getServletContext().getContextPath() + "/login.html";
+        response.sendRedirect(path);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        doGet(request, response);
+    }
+}
