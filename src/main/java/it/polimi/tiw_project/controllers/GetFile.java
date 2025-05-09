e002dc82720560c3e0e2192585c4132268b21090
mssuperlol <mssuperlol@gmail.com>
2025-05-09 18:55:51 +0200

    Initial implementation of song.html

---
+++ src/main/java/it/polimi/tiw_project/controllers/GetFile.java
@@ -0,0 +1,53 @@
+package it.polimi.tiw_project.controllers;
+
+import it.polimi.tiw_project.beans.User;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.file.Files;
+
+@WebServlet("/GetFile/*")
+public class GetFile extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    String folderPath = "";
+
+    @Override
+    public void init() throws ServletException {
+        folderPath = getServletContext().getInitParameter("musicPath");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String loginPath = getServletContext().getContextPath() + "/login.html";
+        HttpSession session = request.getSession();
+
+        if (session.isNew() || session.getAttribute("user") == null) {
+            response.sendRedirect(loginPath);
+            return;
+        }
+
+        User user = (User) session.getAttribute("user");
+        String filename = request.getParameter("filename");
+
+        File file = new File(folderPath + user.getId() + File.separator, filename);
+        System.out.println(filename);
+
+        if (!file.exists() || file.isDirectory()) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND, "File not present");
+            return;
+        }
+
+        response.setHeader("Content-Type", getServletContext().getMimeType(filename));
+        response.setHeader("Content-Length", String.valueOf(file.length()));
+        response.setHeader("Content-Disposition", "inline; filename=\"" + file.getName() + "\"");
+
+        Files.copy(file.toPath(), response.getOutputStream());
+    }
+}
---
+++ src/main/java/it/polimi/tiw_project/controllers/GoToSong.java
@@ -0,0 +1,121 @@
+package it.polimi.tiw_project.controllers;
+
+import it.polimi.tiw_project.beans.Song;
+import it.polimi.tiw_project.beans.User;
+import it.polimi.tiw_project.dao.SongDAO;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.UnavailableException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.WebApplicationTemplateResolver;
+import org.thymeleaf.web.servlet.JakartaServletWebApplication;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@WebServlet("/Song")
+public class GoToSong extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private TemplateEngine templateEngine;
+    private Connection connection = null;
+
+    public GoToSong() {
+        super();
+    }
+
+    @Override
+    public void init() throws UnavailableException {
+        ServletContext servletContext = getServletContext();
+        JakartaServletWebApplication webApplication = JakartaServletWebApplication.buildApplication(servletContext);
+        WebApplicationTemplateResolver templateResolver = new WebApplicationTemplateResolver(webApplication);
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        this.templateEngine = new TemplateEngine();
+        this.templateEngine.setTemplateResolver(templateResolver);
+        templateResolver.setSuffix(".html");
+
+        try {
+            ServletContext context = getServletContext();
+            String driver = context.getInitParameter("dbDriver");
+            String url = context.getInitParameter("dbUrl");
+            String user = context.getInitParameter("dbUser");
+            String password = context.getInitParameter("dbPassword");
+            Class.forName(driver);
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (ClassNotFoundException e) {
+            throw new UnavailableException("Can't load database driver");
+        } catch (SQLException e) {
+            throw new UnavailableException("Couldn't get db connection");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String loginPath = getServletContext().getContextPath() + "/login.html";
+        String homePath = getServletContext().getContextPath() + "/Homepage";
+        HttpSession session = request.getSession();
+
+        if (session.isNew() || session.getAttribute("user") == null) {
+            response.sendRedirect(loginPath);
+            return;
+        }
+
+        User user = (User) session.getAttribute("user");
+        int playlistId, songsIndex, songId;
+
+        try {
+            playlistId = Integer.parseInt(request.getParameter("playlistId"));
+            songsIndex = Integer.parseInt(request.getParameter("songsIndex"));
+            songId = Integer.parseInt(request.getParameter("songId"));
+        } catch (NumberFormatException e) {
+            response.sendRedirect(homePath);
+            return;
+        }
+
+        SongDAO songDAO = new SongDAO(connection);
+        Song currSong;
+
+        try {
+            currSong = songDAO.getSong(songId);
+            System.out.println(currSong);
+            System.out.println(currSong.getUser_id() + " " + user.getId());
+
+            if (currSong == null || currSong.getUser_id() != user.getId()) {
+                response.sendRedirect(homePath);
+                return;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        String path = "/WEB-INF/song.html";
+        JakartaServletWebApplication webApplication = JakartaServletWebApplication.buildApplication(getServletContext());
+        WebContext webC = new WebContext(webApplication.buildExchange(request, response), request.getLocale());
+        webC.setVariable("playlistId", playlistId);
+        webC.setVariable("songsIndex", songsIndex);
+        webC.setVariable("song", currSong);
+        templateEngine.process(path, webC, response.getWriter());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        doGet(request, response);
+    }
+
+    @Override
+    public void destroy() {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException ignored) {
+        }
+    }
+}
