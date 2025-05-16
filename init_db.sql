266298cc9fa152868386e690166ece789db487f7
mssuperlol <mssuperlol@gmail.com>
2025-05-16 11:22:09 +0200

    Replaced static initial values with .tsv files

--- init_db.sql
+++ init_db.sql
@@ -14,6 +14,14 @@ create table users
     primary key (id)
 );
 
+load data local infile 'docs/users.tsv'
+    into table users
+    fields terminated by '\t'
+    enclosed by '\t'
+    lines terminated by '\n'
+    ignore 1 lines
+    (id, username, password, name, surname);
+
 create table genres
 (
     name varchar(32),
@@ -21,11 +29,12 @@ create table genres
 );
 
 #source: https://en.wikipedia.org/wiki/List_of_music_genres_and_styles
-load data local infile 'docs/genres.csv'
+load data local infile 'docs/genres.tsv'
     into table genres
-    fields terminated by ','
-    enclosed by ','
+    fields terminated by '\t'
+    enclosed by '\t'
     lines terminated by '\n'
+    ignore 1 lines
     (name);
 
 create table songs
@@ -40,82 +49,107 @@ create table songs
     genre           varchar(64)  not null,
     music_file_name varchar(128) not null,
     primary key (id),
-    foreign key (user_id) references users (id),
-    foreign key (genre) references genres (name),
+    foreign key (user_id) references users (id) on update cascade on delete no action,
+    foreign key (genre) references genres (name) on update cascade on delete no action,
     unique (user_id, music_file_name)
 );
+
+load data local infile 'docs/songs.tsv'
+    into table songs
+    fields terminated by '\t'
+    enclosed by '\t'
+    lines terminated by '\n'
+    ignore 1 lines
+    (id, user_id, title, image_file_name, album_title, performer, year, genre, music_file_name);
 
 create table playlists
 (
     id      int auto_increment,
     user_id int         not null,
     title   varchar(64) not null,
-    date    date        not null,
+    date    date        not null default current_date,
     primary key (id),
     unique (user_id, title)
 );
+
+load data local infile 'docs/playlists.tsv'
+    into table playlists
+    fields terminated by '\t'
+    enclosed by '\t'
+    lines terminated by '\n'
+    ignore 1 lines
+    (id, user_id, title, @creation_date)
+    SET date = STR_TO_DATE(@creation_date, '%Y-%m-%d');
 
 create table playlist_contents
 (
     playlist int,
     song     int,
     primary key (playlist, song),
-    foreign key (playlist) references playlists (id),
-    foreign key (song) references songs (id)
+    foreign key (playlist) references playlists (id) on update cascade on delete no action,
+    foreign key (song) references songs (id) on update cascade on delete no action
 );
 
-insert into users (username, password, name, surname)
-values ('mssuperlol', 'pass', 'Michele', 'Sangaletti'),
-       ('john', 'word', 'John', 'DarkSouls');
+load data local infile 'docs/playlist_contents.tsv'
+    into table playlist_contents
+    fields terminated by '\t'
+    enclosed by '\t'
+    lines terminated by '\n'
+    ignore 1 lines
+    (playlist, song);
 
-insert into songs (user_id, title, image_file_name, album_title, performer, year, genre, music_file_name)
-values ('1', 'Lovesick - 80,000 Lightyears', 'Omori_Portrait.png', 'OMORI OST', 'Omocat', '2020', 'Electronic',
-        'OMORI OST - 025 Lovesick - 80,000 Lightyears.mp3'),
-       ('1', 'Three Bar Logos', 'Omori_Portrait.png', 'OMORI OST', 'Omocat', '2020', 'Electronic',
-        'OMORI OST - 027 Three Bar Logos.mp3'),
-       ('1', 'Stardust Diving', 'Omori_Portrait.png', 'OMORI OST', 'Omocat', '2020', 'Electronic',
-        'OMORI OST - 032 Stardust Diving.mp3'),
-       ('1', 'You Were Wrong. Go Back', 'Omori_Portrait.png', 'OMORI OST', 'Omocat', '2020', 'Electronic',
-        'OMORI OST - 040 You Were Wrong. Go Back..mp3'),
-       ('1', 'Locked Girl ~ The Girls Secret Room', 'Touhou.jpg', 'Touhou 6 OST', 'Zun', '2002', 'Electronic',
-        'EoSD Stage 4 Boss - Patchouli Knowledges Theme - Locked Girl ~ The Girls Secret Room.mp3'),
-       ('1', 'A Stroll Through Nostalgia', 'Phyrnna.jpg', 'Epic Battle Fantasy 4 OST', 'Phyrnna', '2013', 'Electronic',
-        'Phyrnna - A Stroll Through Nostalgia (Ending Version).mp3'),
-       ('1', 'Shizuka', 'Crosscode.jpg', 'Crosscode OST', 'Deniz Akbulut', '2018', 'Electronic',
-        'Shizuka ~ CrossCode (Original Game Soundtrack).mp3'),
-       ('1', 'The Leopard''s Bane', 'Phyrnna.jpg', 'Epic Battle Fantasy 5 OST', 'Phyrnna', '2018', 'Electronic',
-        'The Leopard''s Bane.mp3'),
-       ('1', 'BAD Apple!!', 'Th04cover.jpg', 'Touhou 4 OST', 'Zun', '1998', 'Electronic',
-        'Touhou 4 - Music #07 - BAD Apple!!.mp3'),
-       ('1', 'Ornstein and Smough', 'Dark Souls.jpg', 'Dark Souls OST', 'Motoi Sakuraba', '2011', 'Electronic',
-        'Ornstein and Smough - Dark Souls Soundtrack 15.mp3'),
-       ('1', 'Windy and Ripply', 'Sonic_Adventure.png', 'Sonic Adventure OST', 'Jun Senoue', '1998', 'Electronic',
-        'Windy and Ripply (Emerald Coast) - Sonic Adventure [OST].mp3'),
-       ('1', 'Command 2', 'Systemshock2box.jpg', 'System Shock 2 OST', 'Josh Randall', '1999', 'Electronic',
-        'System Shock 2 OST Command 2.mp3'),
-       ('1', 'Intro', 'Sysshock.jpg', 'System Shock OST', 'Greg LoPiccolo', '1994', 'Electronic',
-        'System Shock Soundtrack - 00 - Intro.mp3');
-
-insert into playlists (user_id, title, date)
-values (1, 'Videogames OST', '2025/02/13'),
-       (1, 'Omori OST', '2023/01/01'),
-       (2, 'Empty', '2025/01/01');
-
-insert into playlist_contents (playlist, song)
-values (1, 1),
-       (1, 2),
-       (1, 3),
-       (1, 4),
-       (1, 5),
-       (1, 6),
-       (1, 7),
-       (1, 8),
-       (1, 9),
-       (1, 10),
-       (1, 11),
-       (1, 12),
-       (1, 13),
-       (2, 1),
-       (2, 2),
-       (2, 3),
-       (2, 4);
+# insert into users (username, password, name, surname)
+# values ('mssuperlol', 'pass', 'Michele', 'Sangaletti'),
+#        ('john', 'word', 'John', 'DarkSouls');
+#
+# insert into songs (user_id, title, image_file_name, album_title, performer, year, genre, music_file_name)
+# values ('1', 'Lovesick - 80,000 Lightyears', 'Omori_Portrait.png', 'OMORI OST', 'Omocat', '2020', 'Electronic',
+#         'OMORI OST - 025 Lovesick - 80,000 Lightyears.mp3'),
+#        ('1', 'Three Bar Logos', 'Omori_Portrait.png', 'OMORI OST', 'Omocat', '2020', 'Electronic',
+#         'OMORI OST - 027 Three Bar Logos.mp3'),
+#        ('1', 'Stardust Diving', 'Omori_Portrait.png', 'OMORI OST', 'Omocat', '2020', 'Electronic',
+#         'OMORI OST - 032 Stardust Diving.mp3'),
+#        ('1', 'You Were Wrong. Go Back', 'Omori_Portrait.png', 'OMORI OST', 'Omocat', '2020', 'Electronic',
+#         'OMORI OST - 040 You Were Wrong. Go Back..mp3'),
+#        ('1', 'Locked Girl ~ The Girls Secret Room', 'Touhou.jpg', 'Touhou 6 OST', 'Zun', '2002', 'Electronic',
+#         'EoSD Stage 4 Boss - Patchouli Knowledges Theme - Locked Girl ~ The Girls Secret Room.mp3'),
+#        ('1', 'A Stroll Through Nostalgia', 'Phyrnna.jpg', 'Epic Battle Fantasy 4 OST', 'Phyrnna', '2013', 'Electronic',
+#         'Phyrnna - A Stroll Through Nostalgia (Ending Version).mp3'),
+#        ('1', 'Shizuka', 'Crosscode.jpg', 'Crosscode OST', 'Deniz Akbulut', '2018', 'Electronic',
+#         'Shizuka ~ CrossCode (Original Game Soundtrack).mp3'),
+#        ('1', 'The Leopard''s Bane', 'Phyrnna.jpg', 'Epic Battle Fantasy 5 OST', 'Phyrnna', '2018', 'Electronic',
+#         'The Leopard''s Bane.mp3'),
+#        ('1', 'BAD Apple!!', 'Th04cover.jpg', 'Touhou 4 OST', 'Zun', '1998', 'Electronic',
+#         'Touhou 4 - Music #07 - BAD Apple!!.mp3'),
+#        ('1', 'Ornstein and Smough', 'Dark Souls.jpg', 'Dark Souls OST', 'Motoi Sakuraba', '2011', 'Electronic',
+#         'Ornstein and Smough - Dark Souls Soundtrack 15.mp3'),
+#        ('1', 'Windy and Ripply', 'Sonic_Adventure.png', 'Sonic Adventure OST', 'Jun Senoue', '1998', 'Electronic',
+#         'Windy and Ripply (Emerald Coast) - Sonic Adventure [OST].mp3'),
+#        ('1', 'Command 2', 'Systemshock2box.jpg', 'System Shock 2 OST', 'Josh Randall', '1999', 'Electronic',
+#         'System Shock 2 OST Command 2.mp3'),
+#        ('1', 'Intro', 'Sysshock.jpg', 'System Shock OST', 'Greg LoPiccolo', '1994', 'Electronic',
+#         'System Shock Soundtrack - 00 - Intro.mp3');
+#
+# insert into playlists (user_id, title, date)
+# values (1, 'Videogames OST', '2025/02/13'),
+#        (1, 'Omori OST', '2023/01/01'),
+#        (2, 'Empty', '2025/01/01');
+#
+# insert into playlist_contents (playlist, song)
+# values (1, 1),
+#        (1, 2),
+#        (1, 3),
+#        (1, 4),
+#        (1, 5),
+#        (1, 6),
+#        (1, 7),
+#        (1, 8),
+#        (1, 9),
+#        (1, 10),
+#        (1, 11),
+#        (1, 12),
+#        (1, 13),
+#        (2, 1),
+#        (2, 2),
+#        (2, 3),
+#        (2, 4);
